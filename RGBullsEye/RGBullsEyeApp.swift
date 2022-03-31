9d5d028e5a5bafa22b80beaf778a37b9cf915cad
NyiYeHan <39417215+NyiYeHan@users.noreply.github.com>
2022-03-31 09:43:31 +0630

    Initial Commit

---
+++ RGBullsEye/RGBullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBullsEyeApp.swift
+//  RGBullsEye
+//
+//  Created by Nyi Ye Han on 31/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
